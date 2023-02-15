9ffa87933af8ea141bce14adccdcb52409f02931
Grzegorz Wilczynski <grzegorz.wilczynski@infopark.de>
2023-02-15 14:04:31 +0100

    convert png to webp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import radio357Logo from "./assets/357.png";
-import radioNowySwiatLogo from "./assets/ns.png";
-import radioNewonceLogo from "./assets/no.png";
+import radio357Logo from "./assets/357.webp";
+import radioNowySwiatLogo from "./assets/ns.webp";
+import radioNewonceLogo from "./assets/no.webp";
 
 function App() {
   return (
@@ -10,21 +10,21 @@ function App() {
         <h1>Radio Hub</h1>
         <ul className="SongListItems">
           <li className="SongListItem selected">
-            <img className="songCover" src={radio357Logo} alt="cover"/>
+            <img className="songCover" src={radio357Logo} alt="cover" />
             <audio controls>
-              <source src="https://stream.rcs.revma.com/ye5kghkgcm0uv"/>
+              <source src="https://stream.rcs.revma.com/ye5kghkgcm0uv" />
             </audio>
           </li>
           <li className="SongListItem selected">
-            <img className="songCover" src={radioNowySwiatLogo} alt="cover"/>
+            <img className="songCover" src={radioNowySwiatLogo} alt="cover" />
             <audio controls>
-              <source src="https://stream.rcs.revma.com/ypqt40u0x1zuv"/>
+              <source src="https://stream.rcs.revma.com/ypqt40u0x1zuv" />
             </audio>
           </li>
           <li className="SongListItem selected">
-            <img className="songCover" src={radioNewonceLogo} alt="cover"/>
+            <img className="songCover" src={radioNewonceLogo} alt="cover" />
             <audio controls>
-              <source src="https://stream.rcs.revma.com/fq577gymev8uv"/>
+              <source src="https://stream.rcs.revma.com/fq577gymev8uv" />
             </audio>
           </li>
         </ul>
